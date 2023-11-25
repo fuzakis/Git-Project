b5f471fbad20f65baa6e226d026a69686624adc2
fuzakis <fuzakiduty@gmail.com>
2023-11-25 14:00:35 +0700

    tambah ffmpeg

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Kode audio editing di sini
+    const appElement = document.getElementById('app');
+
+    // Contoh: Tampilkan formulir pemilihan aksi
+    appElement.innerHTML = `
+        <label>Select Action:</label>
+        <select id="action">
+            <option value="cut">Cut Audio</option>
+            <option value="merge">Merge Audio</option>
+            <!-- Tambahkan opsi lainnya sesuai kebutuhan -->
+        </select>
+        <br>
+        <input type="file" id="audioFile" accept="audio/*">
+        <button onclick="performAction()">Perform Action</button>
+        <div id="result"></div>
+    `;
+
+    // Contoh: Fungsi untuk melakukan aksi
+    window.performAction = async () => {
+        const action = document.getElementById('action').value;
+        const audioFile = document.getElementById('audioFile').files[0];
+
+        if (!audioFile) {
+            alert('Please select an audio file.');
+            return;
+        }
+
+        const resultElement = document.getElementById('result');
+        resultElement.innerHTML = 'Processing...';
+
+        try {
+            // Lakukan pemrosesan audio menggunakan ffmpeg.js
+            // Implementasi kode ini tergantung pada aksi yang dipilih
+            // ...
+
+            resultElement.innerHTML = 'Action completed successfully!';
+        } catch (error) {
+            console.error('Error:', error);
+            resultElement.innerHTML = 'Error processing audio.';
+        }
+    };
 });
